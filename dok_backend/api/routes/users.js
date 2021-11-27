88f021c5ed59e68cf663b19f225522cdfefc05c2
Allister <aj@definitive.co.nz>
2021-11-27 15:40:13 +1300

    Made user registration res more usable for frontend

--- dok_backend/api/routes/users.js
+++ dok_backend/api/routes/users.js
@@ -1,98 +1,108 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const passport = require('passport')
+const passport = require('passport');
 
-const User = require('../models/user')
+const User = require('../models/user');
 
 const key = process.env.KEY;
 const router = express.Router();
 
-router.post('/register', async function(req, res){
+//# /users
 
-  // Data from the POST
-  let {
-    username,
-    email,
-    password,
-    confirm_password
-  } = req.body
-
-  // Check to see if passwords match
-  if(password !== confirm_password){return res.status(400).json({msg: "Passwords do not match"})} 
+//## POST /register
+router.post('/register', async (req, res) => {
+  cred_res = {
+    username: false,
+    email: false,
+    password: false,
+    confirm_password: false,
+  };
 
   // Check to see if the username is taken
-  const isUser = await User.findOne({"username": username})
-  if (isUser) {
-    return res.status(400).json({
-      msg: "Username already registered : " + username
-    })
-  }
+  await User.findOne({ username: req.body.username })
+    .exec()
+    .then((isUser) => {
+      if (!isUser) cred_res.username = true;
+    });
 
   // Check to see if the email is already registered
-  const isEmail = await User.findOne({"email": email})
-  if (isEmail) {
-    return res.status(400).json({
-      msg: "Email already registered : " + email
-    })
+  await User.findOne({ email: req.body.email })
+    .exec()
+    .then((isEmail) => {
+      if (!isEmail) cred_res.email = true;
+    });
+
+  //todo Add password requirements
+  cred_res.password = true;
+
+  // Check to see if passwords match
+  if (req.body.password === req.body.confirm_password) {
+    cred_res.confirm_password = true;
   }
-  
+
+  if (!Object.values(cred_res).every((value) => value === true)) {
+    return res.status(400).json({
+      success: false,
+      msg: cred_res,
+    });
+  }
+
   const newUser = new User({
-      username,
-      email,
-      password
-  })
-  
+    username: req.body.username,
+    email: req.body.email,
+    password: null,
+  });
+
   // Encrypt and send
   bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash(newUser.password, salt, (err, hash) => {
-      if(err) throw err;
-  
+    bcrypt.hash(req.body.password, salt, (err, hash) => {
       newUser.password = hash;
-      newUser.save().then(user => {
+      newUser.save().then(() => {
         return res.status(201).json({
           success: true,
-          msg: "User is now registered"
-        })
-      })
-    })
-  })
-})
+          msg: 'User is now registered',
+        });
+      });
+    });
+  });
+});
 
-router.post('/login', async function(req, res){
-  
+//## POST /login
+router.post('/login', async function (req, res) {
   // Check to see if the username exists
-  User.findOne({"username": req.body.username}).then(user => {
-    if(!user){
+  User.findOne({ username: req.body.username }).then((user) => {
+    if (!user) {
       return res.status(404).json({
-        success: false, 
-        msg:"Username not found"
-      })
-    }  
+        success: false,
+        msg: 'Username not found',
+      });
+    }
 
     // Check to see it the passwords match
-    bcrypt.compare(req.body.password, user.password).then(isMatch => {
-      if(isMatch){
+    bcrypt.compare(req.body.password, user.password).then((isMatch) => {
+      if (isMatch) {
         const payload = {
           _id: user._id,
           username: user.username,
-          email: user.email
-        }
-    
+          email: user.email,
+        };
+
         jwt.sign(payload, key, (err, token) => {
           res.status(200).json({
             success: true,
-            msg: "You are logged in!",
-            token: token
-          })
-        })
+            msg: 'You are logged in!',
+            token: token,
+          });
+        });
+      } else {
+        return res.status(404).json({
+          success: false,
+          msg: 'Incorrect password',
+        });
       }
-      else{return res.status(404).json({
-        success: false, 
-        msg:"Incorrect password"}
-      )}
-    })
-  })
-})
+    });
+  });
+});
 
 module.exports = router;
