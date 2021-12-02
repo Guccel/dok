8190c6491e9687d5bb6d8d9a6ded7eef8ff85f47
sweepychild <rapinnz@gmail.com>
2021-12-02 14:16:28 +1300

    added nextjs

---
+++ backend/api/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    default: "User"
+  },
+  username: {
+    type: String, 
+    required: true
+  },
+  email: {
+    type: String, 
+    required: true
+  },
+  password: {
+    type: String, 
+    required: true
+  },
+  date: {
+    type: Date, 
+    default: Date.now
+  }
+})
+
+module.exports = User = mongoose.model('users', UserSchema);
