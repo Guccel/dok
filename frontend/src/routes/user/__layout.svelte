512851f4caea85a096bcedea4a724d8741703c11
sweepychild <rapinnz@gmail.com>
2022-01-18 11:05:52 +1300

    login/register page layout

---
+++ frontend/src/routes/user/__layout.svelte
@@ -0,0 +1,14 @@
+<script context="module">
+	import { session } from '$app/stores';
+</script>
+
+{#if $session.user.authenticated == false}
+  <nav>
+    <a href="login">Login</a>
+    <a href="register">Register</a>
+  </nav>
+{/if}
+
+<main>
+	<slot />
+</main>
