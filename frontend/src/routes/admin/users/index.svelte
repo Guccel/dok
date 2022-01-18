1b4f17f7a71065bc1d2264654fa240a067bebb09
sweepychild <rapinnz@gmail.com>
2022-01-18 13:04:46 +1300

    fixed loading user data on admin page

--- frontend/src/routes/admin/users/index.svelte
+++ frontend/src/routes/admin/users/index.svelte
@@ -1,36 +1,29 @@
-<script context="module">
-	export async function load() {
-		const response = await axios({
-			method: 'POST',
-			url: 'http://localhost:3000/user',
-			headers: { 'Content-Type': 'application/json' },
-			data: { method: 'all' }
-		});
-
-		return {
-			props: {
-				users: response.data.users
-			}
-		};
-	}
-</script>
-
 <script lang="ts">
 	import axios from 'axios';
 
 	import type { User_Type } from 'src/global';
+	import { onMount } from 'svelte';
 
-	export let users: Array<User_Type>;
+	export let users = [];
 
 	async function updateUser(i: number) {
 		const user = users[i];
-		const response = await axios({
+		await axios({
 			method: 'PATCH',
 			url: `http://localhost:3000/user/patch/${user._id}`,
 			headers: { 'Content-Type': 'application/json' },
 			data: user
 		});
 	}
+
+	onMount(async () => {
+		return await axios({
+			method: 'POST',
+			url: 'http://localhost:3000/user',
+			headers: { 'Content-Type': 'application/json' },
+			data: { method: 'all' }
+		}).then((res) => {users = res.data._ids; console.log(users)})
+	})
 </script>
 
 <table>
@@ -39,21 +32,25 @@
 		<td>email</td>
 		<td>type</td>
 	</tr>
-	{#each users as user, i}
-		<tr>
-			<td>{user.username}</td>
-			<td>{user.email}</td>
-			<td
-				><select
-					bind:value={user.type}
-					on:change={() => {
-						updateUser(i);
-					}}
-				>
-					<option value="user">user</option>
-					<option value="admin">admin</option>
-				</select>
-			</td>
-		</tr>
-	{/each}
+	{#await users}
+		<p> Loading user data </p>
+	{:then data}
+		{#each data as user, i}
+			<tr>
+				<td>{user.username}</td>
+				<td>{user.email}</td>
+				<td>
+					<select
+						bind:value={user.type}
+						on:change={() => {
+							updateUser(i);
+						}}
+					>
+						<option value="user">user</option>
+						<option value="admin">admin</option>
+					</select>
+				</td>
+			</tr>
+		{/each}
+	{/await}
 </table>
