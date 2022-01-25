7434ae42d4d0a1475707ee25e7809bdfb5173ad9
Guccel <aj@definitive.co.nz>
2022-01-26 11:24:11 +1300

    asdfasdf

--- frontend/src/routes/admin/todo/index.svelte
+++ frontend/src/routes/admin/todo/index.svelte
@@ -1,4 +1,28 @@
-<script>
+<script context="module">
+	export async function load() {
+		const response = await axios({
+			method: 'POST',
+			url: 'http://localhost:3000/todo',
+			headers: { 'Content-Type': 'application/json' },
+			data: {}
+		});
 
+		return {
+			props: {
+				todo_ids: response.data._ids
+			}
+		};
+	}
 </script>
 
+<script lang="ts">
+	import axios from 'axios';
+
+	export let todo_ids: Array<string>;
+</script>
+
+{#key todo_ids}
+	{#each todo_ids as id}
+		{JSON.stringify(id)}
+	{/each}
+{/key}
